5e5a0233e1b54f0f770209ff63619308b6d29e81
enf69 <everth.navarro@ing.uab.edu.bo>
2022-06-23 15:49:11 -0400

    subiendo

---
+++ README.md
@@ -0,0 +1,47 @@
+Obervaciones.
+Los siguientes pasos nos ayudan a crear de manera utomatica con selenium-webdriver, hasta llegar a recaptcha, ya que con
+selenium no se realizar esta accion.
+
+//Titulo
+Verificar automatizacion registro de nuevo usuario en Twitch hasta lleagar a recaptcha.
+
+Prerrequisitos.
+1.-Dispositivo electronico(Movil, Laptop, Pc, etc),
+2.-Conexion a internet.
+3.-Navegador web(Chrome, Edge, Firefox, etc).
+4.-Correo electronico valido.
+
+
+Pasos.
+//Instalacion de herramientas.
+1.-Ir al sitio web de visual studio code: https://code.visualstudio.com/
+2.-Hacer clic en Download.
+3.-Ir a la carpeta donde se descago el archivo.
+4.-Hacer clic decho, seleccionar abrir y seguir los pasos(Siguiente).
+5.-Ir al sitio web de node.js: https://nodejs.org/es/
+6.-Hacer clic en 16.15.1 LTS.
+7.-Ir a la carpeta donde se descargo el archivo.
+8.-Hacer clic derecho, seleccionar abrir y seguir los pasos(Siguiente).
+9.-Buscar la carpeta bin del archivo general de Node.js.
+10.-Copiar la direccion del archivo bin y dirigirse a Variables de entorno.
+11.-Pegar la direccion del archivo bin, pegar en Path, guardar y salir.
+
+//Empezando con la automatizacion.
+12.-Descargar chromedriver.exe y geckodriver.exe.
+13.-Guardar en una carpeta ambos y copiar la direccion de la carpeta.
+14.-Pegar la direccion en variables de entorno, en el espacio Path.
+15.-En la misma carpeta creamos una carpeta donde haremos nuestro automatizacion.
+16.-Abrir con cmd.
+17.-Escribir npm init.
+18.-Escribir npm init -y.
+19.-Escribir code . para abrir con visual studio code.
+20.-Abrir la terminal en visual studio code.
+21.-Escribir npm install selenium-webdriver.
+22.-Crear un archivo js "index.js".
+
+
+1.-git init
+2.-git add .
+3.-git commit -m "first commit"
+4.-git remote add origin https://github.com/enf69/automatizacion-t.git
+5.-git push -u origin master
---
+++ index.js
@@ -0,0 +1,55 @@
+//Inicio de sesion en linkdin con una cuneta creada
+//llamamos a la dependendia
+const {Builder, By, Key, until} = require("selenium-webdriver");
+
+//
+(async function example() {
+  let driver = await new Builder().forBrowser("firefox").build();
+  try {
+    //accedemos a la pagina principal de linkedin
+    await driver.get('https://www.twitch.tv');
+    // buscando y accediendo a inicio de sesion
+    await driver.findElement(By.xpath('/html/body/div[1]/div/div[2]/nav/div/div[3]/div[3]/div/div[1]/div[2]/button/div/div')).click();
+    await driver.sleep(5000);
+
+    // llenando formulario 
+    //usuario
+    let usua = driver.findElement({xpath:'//*[@id="signup-username"]'});
+    await driver.sleep(4000);
+    usua.sendKeys('Everth69696900');
+    //contraseña
+    let passwd = driver.findElement({xpath:'//*[@id="password-input"]'});
+    await driver.sleep(2000);
+    passwd.sendKeys('$$$Everth_1999');
+    //contraseña
+    let passwdconf = driver.findElement({xpath:'//*[@id="password-input-confirmation"]'});
+    await driver.sleep(2000);
+    passwdconf.sendKeys('$$$Everth_1999');
+    //dia nacimiento
+    let dianac = driver.findElement({xpath:'/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div[3]/form/div/div[3]/div/div[2]/div[1]/div/input'});
+    await driver.sleep(2000);
+    dianac.sendKeys('26');
+    //mes nacimiento
+    await driver.findElement(By.xpath('/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div[3]/form/div/div[3]/div/div[2]/div[2]/select/option[7]')).click();
+    await driver.sleep(2000);
+    //año nacimiento
+    let anionac = driver.findElement({xpath:'/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div[3]/form/div/div[3]/div/div[2]/div[3]/div/input'});
+    await driver.sleep(1000);
+    anionac.sendKeys('2000');
+    //año nacimiento
+    let telefono = driver.findElement({xpath:'//*[@id="phone-input"]'});
+    await driver.sleep(2000);
+    telefono.sendKeys('63885630');
+    const btn = await driver.findElement(By.xpath('/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div[3]/form/div/div[5]/button/div/div'));
+        //scroll del raton 
+        driver.executeScript('arguments[0].scrollIntoView(0,300);', btn);
+        await driver.sleep(4000);
+        btn.click();
+        await driver.sleep(2000);
+
+    //finalizamos la tarea
+  }finally {
+    await driver.quit();
+  }
+})
+();
